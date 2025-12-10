685ed523ff8a20f1bd7f07073c86fee38a35f27c
AidenCM1234 <aidenm124@nycstudents.net>
2025-12-10 09:26:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,5 @@
-import requests
-
-def getPlayer():
-    response = requests.get(f"curl -v https://api.chess.com/pub/player/{player}")
-    if response.status_code != 200:
-        print("Error fetching data!")
-        return None
-    
-    data = response.json()
-    return {
-        "name": data["name"],
-        "height": data["height"],
-        "weight": data["weight"],
-        "types": [t["type"]["name"] for t in data["types"]]
-    }
-getPlayer("hikaru")
-player = getPlayer("hikaru")
-print(player)
+import  requests
+def test():
+    response = requests.get('https://api.chess.com/pub/player/hikaru')
+    print(response)
+test()
