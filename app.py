bd95016e897c6a85ad547e42af0d4b36eb3038f6
AidenCM1234 <aidenm124@nycstudents.net>
2025-12-11 09:26:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,26 @@
-import  requests
-def test():
-    response = requests.get('https://api.chess.com/pub/player/hikaru')
-    print(response)
-test()
+import requests
+
+def getPoke(poke):
+    url = f"https://api.chess.com/pub/player/{poke.lower()}"
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        print("Error fetching data!")
+        return None
+    
+    avatar_usage =False
+    data = response.json()
+    for i in data:
+        if i == "avatar":
+            avatar_usage =True
+    return {
+        "username": data["username"], 
+        if avatar_usage==True:
+            "avatar": data["avatar"] ,
+        "followers": data["followers"]
+    }
+
+pokemon = getPoke("bulbasaur")
+print(pokemon)
