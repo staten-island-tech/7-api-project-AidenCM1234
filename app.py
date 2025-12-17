4661f87feb17e53a985eceed774eb463f6addbfe
AidenCM1234 <aidenm124@nycstudents.net>
2025-12-17 09:26:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import requests
 
-def getPlayers(user):
+def getPlayers(user,userb):
     url = f"https://api.chess.com/pub/player/{user.lower()}"
-    headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
     response = requests.get(url, headers=headers)
     if response.status_code != 200:
         print("Error fetching data!")
@@ -11,8 +10,7 @@ def getPlayers(user):
     data = response.json()
 
     url2 = f"https://api.chess.com/pub/player/{user.lower()}/stats"
-    headers2 = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+    headers2 = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
     response2 = requests.get(url2, headers=headers2)
     if response2.status_code != 200:
         print("Error fetching data!")
@@ -21,17 +19,12 @@ def getPlayers(user):
 
     avatar_usage =False
     for i in data:
-            print(i)
-            print("  ")
             if i =="avatar":
                  avatar_usage=True
     if avatar_usage == True:
          print (f"avatar: {data}[avatar]")
     else:
          print("No avatar")
-    for i in data2:
-            print(i)
-            print("  ")
     one_win=data2["chess_daily"]["record"]["win"]
     one_lose=data2["chess_daily"]["record"]["loss"]
     one_best=data2["chess_daily"]["best"]["rating"]
@@ -41,8 +34,28 @@ def getPlayers(user):
           "1chess_daily_record_win": data2["chess_daily"]["record"]["win"],
           "1chess_daily_record_loss": data2["chess_daily"]["record"]["loss"],
           "1chess_daily_best_rating": data2["chess_daily"]["best"]["rating"]}
-    
-    return basic_infomation_player1
+    ###
+    ###
+    url = f"https://api.chess.com/pub/player/{userb.lower()}"
+    response = requests.get(url, headers=headers)
+    data = response.json()
 
-player = getPlayers("bob")#,"erik")
+    url2 = f"https://api.chess.com/pub/player/{userb.lower()}/stats"
+    response2 = requests.get(url2, headers=headers)
+    data2 = response2.json()
+    two_win=data2["chess_daily"]["record"]["win"]
+    two_lose=data2["chess_daily"]["record"]["loss"]
+    two_best=data2["chess_daily"]["best"]["rating"]
+
+    basic_infomation_player2 = {
+        "2username": data["username"],
+        "2followers": data["followers"],
+        "2avatar": data.get("avatar"),
+        "2chess_daily_record_win": data2["chess_daily"]["record"]["win"],
+        "2chess_daily_record_loss": data2["chess_daily"]["record"]["loss"],
+        "2chess_daily_best_rating": data2["chess_daily"]["best"]["rating"],
+    }
+    return basic_infomation_player1 and basic_infomation_player2
+
+player = getPlayers("bob","erik")
 print(player)
